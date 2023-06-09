b06744f1e1146c97fe83ff4d6af17f0125ad1a25
GuralTOO <guralben@gmail.com>
2023-06-09 19:14:23 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import useSocket from "./useSocket";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [inputText, setInputText] = useState("");
+  const [serverResponse, setServerResponse] = useState("");
+  const socket = useSocket("http://localhost:5000");
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("message", (msg) => {
+        console.log("Received message: ", msg);
+        setServerResponse((prev) => prev + msg);
+      });
+    }
+  }, [socket]);
+
+  const sendTextToServer = () => {
+    setServerResponse("");
+    if (socket && socket.connected) {
+      socket.emit("message", inputText);
+    } else {
+      console.error("Socket is not connected");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Economic Club Chat</h1>
+      <div className="content">
+        <input
+          className="input-text"
+          onChange={(e) => setInputText(e.target.value)}
+          value={inputText}
+          placeholder="Enter your text here"
+        />
+        <button className="button" onClick={sendTextToServer}>
+          Send
+        </button>
+        <div className="response-container">
+          <p>{serverResponse}</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
